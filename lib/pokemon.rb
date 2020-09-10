eb95ad0af2b72cebf5cdd1cb11d055487e91d856
Sean Ransonette <sransonette@gmail.com>
2020-09-10 22:21:15 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -35,7 +35,7 @@ class Pokemon
     data = db.execute(sql, id).flatten
     #binding.pry
     Pokemon.new(id: data[0], name: data[1], type: data[2], db: db)
-    DB[:conn].execute(sql, [id]).flatten
+    #DB[:conn].execute(sql, [id]).flatten
     self.new(id)
 
   end
